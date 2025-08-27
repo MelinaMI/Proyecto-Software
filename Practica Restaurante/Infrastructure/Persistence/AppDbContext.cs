f349e9879f4800b1e5063be354f0bf890d5387a0
Miranda Ibarra Melina <mirandaibarramelina@gmail.com>
2025-08-27 01:18:34 -0300

    creacion del ApplicationDbContext, declaracion de las entidades

---
+++ Practica Restaurante/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        //Declaro las entidades
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<DeliveryType> DeliveryTypes { get; set; }
+        public DbSet<Dish>Dishes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Status> Statuses { get; set; }
+
+
+        //modelo las relaciones
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Relacion 1:1 
+            modelBuilder.Entity<Category>(entity =>
+            {
+               // entity.ToTable("Category"); //nombre de la tabla
+                entity.HasKey(c => c.Id); //PK
+            });
+        }
+
+    }
+}
